2236c3bdb9181e5f9d512a505d21f5781e06b042
Stablesj <93344245+Stablesj@users.noreply.github.com>
2021-11-03 17:17:17 +0000

    plt grayscale not working

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+#%%
+
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2 as cv
 
+image1 = plt.imread('photo 1.jpg', 'gray')
+image2 = plt.imread('photo 2.jpg')
+
+plt.figure('1')
+plt.imshow(image1); plt.axis("off")
+
+plt.figure('2')
+plt.imshow(image2); plt.axis("off")
+
+# double1 = np.double(image1)
+
+# %%
+img1 = image2-image1
+plt.imshow(img1)
+
+# %%
+keep = np.double(img1>130)
+# img1()
+
+plt.imshow(keep)
+
+
+# %%
