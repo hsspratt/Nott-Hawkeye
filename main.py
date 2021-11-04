8b777a3a5d9a089afcd14615b35aeb9bbe8eff31
hsspratt <spratths@gmail.com>
2021-11-04 13:43:01 +0000

    cv2 greyscale - main.py

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-image1 = plt.imread('photo 1.jpg', 'gray')
-image2 = plt.imread('photo 2.jpg')
+f_image1 = 'photo 1.jpg'
+f_image2 = 'photo 2.jpg'
+image1 = plt.imread(f_image1, 'gray')
+image2 = plt.imread(f_image2)
 
 plt.figure('1')
 plt.imshow(image1); plt.axis("off")
 
 plt.figure('2')
 plt.imshow(image2); plt.axis("off")
+
+img1 = cv.imread(f_image1,0)
+img2 = cv.imread(f_image2,0)
+
+cv.imshow(f_image1,img1)
+cv.imshow(f_image2,img2)
+
+cv.waitKey(10)
+
+cv.destroyAllWindows()
+
 
 # double1 = np.double(image1)
 
