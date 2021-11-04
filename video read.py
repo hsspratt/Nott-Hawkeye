75534c24fb8e633e515503e5ad269dd473db143d
Stablesj <93344245+Stablesj@users.noreply.github.com>
2021-11-04 19:03:10 +0000

    video reader 1

--- video read.py
+++ video read.py
@@ -1,17 +1,40 @@
-
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-cap = cv2.VideoCapture('/Physics Pics/IMG_0600.mov')
-count = 0
-while cap.isOpened():
-    ret,frame = cap.read()
-    cv2.imshow('window-name', frame)
-    cv2.imwrite("frame%d.jpg" % count, frame)
-    count = count + 1
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+# cap = cv.VideoCapture('/Physics Pics/IMG_0600.mov')
+
+cap = cv.VideoCapture('cars 10s 1.mp4')
+
+if (cap.isOpened() == False):
+    print('Error opening video')
+
+# ret, frame = cap.read()
+# plt.imshow(frame)
+
+# image1 = cv.imread("A3-P1.jpeg")
+# plt.imshow(image1)
+
+
+while (cap.isOpened()):
+    # capture frame by frame
+    ret, frame = cap.read()
+    if ret == True:
+        # display frame
+        # plt.figure()
+        # plt.imshow(frame)
+        cv.imshow('Frames', frame)
+        
+    # Press Q on keyboard to  exit
+    # if cv2.waitKey(25) & 0xFF == ord('q'):
+    #     break
+
+    else:
         break
 
 cap.release()
-cv2.destroyAllWindows() # destroy all opened windows
+
+
+
+# %%
