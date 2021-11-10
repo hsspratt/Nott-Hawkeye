83e8840d165583d5f1859ea2b7a5cba0638ef1e7
Stablesj <93344245+Stablesj@users.noreply.github.com>
2021-11-10 00:22:08 +0000

    9/11.4 morph closing

--- morph.py
+++ morph.py
@@ -1,4 +1,4 @@
-# %% imports
+# %% imports ~20s
 from timeit import default_timer as timer
 tic1 = timer()
 import numpy as np
@@ -6,11 +6,27 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 import pickle
 import bz2
+import skimage.morphology as morph
 
 f_name = "diff_thresh.pbz2"
 
 with bz2.BZ2File(f_name, 'rb') as f:
     diff_thresh = pickle.load(f)
 
+toc1 = timer()
+print(f'completed import in {toc1-tic1: .1f}s')
+# %%
+str_el = morph.disk(8)
+array_shape = np.shape(diff_thresh)
+
+# for i in range 
+im = diff_thresh[:,:,300]
+opened = morph.closing(im, str_el)
+
+plt.figure()
+plt.imshow(im, cmap='gray') 
+
+plt.figure()
+plt.imshow(opened, cmap='gray') 
 
 # %%
