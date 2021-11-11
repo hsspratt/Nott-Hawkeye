3e843574b9da20d4b0f759e0362c0deede66f975
hsspratt <spratths@gmail.com>
2021-11-11 16:11:41 +0000

    function making

--- video_read.py
+++ video_read.py
@@ -17,47 +17,53 @@ def export(f_name_full):
 
 # record video into array ~10s
 
+def vid2array(f_cap):
+
+    # capture video into array
+    cap = cv.VideoCapture(f_cap)
+    fps = int(cap.get(5))
+    nframes = int(cap.get(7))
+    print(f'number of frames: {nframes}')
+
+    if (cap.isOpened() == False):
+        print('Error opening video')
+
+    ret, frame = cap.read()
+
+    plt.figure()
+    image1 = skm.rgb2gray(frame)
+    plt.imshow(image1, cmap='gray')
+    plt.axis('off')
+
+    n = 0
+    video_size = np.hstack((np.shape(frame)[0:2],nframes))
+    video = np.zeros(video_size)
+
+    while (cap.isOpened()):
+        # capture frame by frame
+        ret, frame = cap.read()
+        if ret == True:
+            # save grayscale image
+            video[:,:,n] = skm.rgb2gray(frame)
+
+            n+=1
+            print(" ", end=f"\r frame: {n} ", flush=True)
+
+
+        else:
+            break
+
+    cap.release()
+
+    return video, nframes
+
 # specify file to read from:
 f_cap = 'IMG_0599.mp4'
 
 # specify filename to save to 
 f_name = 'video1'
 
-# capture video into array
-cap = cv.VideoCapture(f_cap)
-fps = int(cap.get(5))
-nframes = int(cap.get(7))
-print(f'number of frames: {nframes}')
-
-if (cap.isOpened() == False):
-    print('Error opening video')
-
-ret, frame = cap.read()
-
-plt.figure()
-image1 = skm.rgb2gray(frame)
-plt.imshow(image1, cmap='gray')
-plt.axis('off')
-
-n = 0
-video_size = np.hstack((np.shape(frame)[0:2],nframes))
-video = np.zeros(video_size)
-
-while (cap.isOpened()):
-    # capture frame by frame
-    ret, frame = cap.read()
-    if ret == True:
-        # save grayscale image
-        video[:,:,n] = skm.rgb2gray(frame)
-
-        n+=1
-        print(" ", end=f"\r frame: {n} ", flush=True)
-
-
-    else:
-        break
-
-cap.release()
+video, nframes = vid2array(f_cap)
 
 plt.figure()
 plot1 = plt.imshow(video[:,:,nframes-50], cmap='gray')
